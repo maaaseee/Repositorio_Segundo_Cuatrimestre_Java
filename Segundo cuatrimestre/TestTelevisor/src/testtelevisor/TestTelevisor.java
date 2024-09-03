ea4de10bc324b1f230b495d033f2a9695d44192e
Federico Aieta <maxe1111@hotmail.com>
2024-09-03 14:20:34 -0300

    Clase 3 con baus

--- Segundo cuatrimestre/TestTelevisor/src/testtelevisor/TestTelevisor.java
+++ Segundo cuatrimestre/TestTelevisor/src/testtelevisor/TestTelevisor.java
@@ -5,6 +5,7 @@ public class TestTelevisor {
     public static void main(String[] args) {
         Televisor tv1 = new Televisor("Samsung", 21.5, "FULLHD", 7, 18, true);
         Televisor tv2 = new Televisor("Sony", 60, "4K", 7, 18, true);
+        Televisor tv3 = new Televisor("Sony", 60, "4K", 7, 18, true);
         
         //System.out.printf("Info televisor 1:\nMarca: %s\nResolucion: %S\nMedidas: %d"
         //        + "\nCanal: %d\nVolumen: %d\nEncendido");
@@ -18,5 +19,10 @@ public class TestTelevisor {
                 "\nResolucion: " + tv2.resolucion + "\nMedidas: " + tv2.medidas
                 + "\nCanal: " + tv2.canal + "\nVolumen: " + tv2.volumen + "\n" +
                 ((tv2.encendido)?"Encendido":"Apagado"));
+        
+        System.out.println("Info Televisor\n\nMarca: " + tv3.marca +
+                "\nResolucion: " + tv3.resolucion + "\nMedidas: " + tv3.medidas
+                + "\nCanal: " + tv3.canal + "\nVolumen: " + tv3.volumen + "\n" +
+                ((tv3.encendido)?"Encendido":"Apagado"));
     }
 }
